e3250d5fc070093ea92dba83d52632c06c511801
Samuel O Rodriguez <SamuelORodriguez@users.noreply.github.com>
2018-09-25 16:39:07 -0400

    BattleShips
    Hello Battleships

---
+++ src/com/sor/Main.java
@@ -0,0 +1,11 @@
+package com.sor;
+
+import com.sor.game.Game;
+
+public class Main {
+
+    public static void main(String args[]) {
+        Game game = new Game();
+        game.start();
+    }
+}
